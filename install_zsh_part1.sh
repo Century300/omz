2d4f9f4b9c2e77a66b6a6f44ee1033ca64296866
Century300 <67885281+Century300@users.noreply.github.com>
2021-11-26 15:01:04 -0600

    Update install_zsh_part1.sh

--- install_zsh_part1.sh
+++ install_zsh_part1.sh
@@ -10,7 +10,7 @@ case "${unameOut}" in
   *)          MACHINE="UNKNOWN:${unameOut}"
 esac
 
-echo "Your machine is" __$MACHINE__''', I used these scripts to install oh-my-zsh and my .zshrc configuration to the Ubuntu machine (Linux) on TryHackMe.com, I have not tested the scripts with other machines yet. You might want to run "sudo apt update && sudo apt upgrade -y" before running this script.'''
+echo "Your machine is" __$MACHINE__''', I used these scripts to install oh-my-zsh and my .zshrc configuration to the Ubuntu machine (Linux) on TryHackMe.com, I have not tested the scripts with other machines yet. You might want to run "sudo apt upgrade -y" after running this 1st bash script, but it might take a long time to upgrade all packages.'''
 
 echo '''Please continue by typing ./install_zsh_part2.sh into the terminal to finsh the installation. I could not combine the 2 scripts because after installing oh-my-zsh on TryHackMe.com attackBox machine, the system will stop and ask if you want to change the default shell to /usr/bin/zsh, and you should type "y" '''
 
